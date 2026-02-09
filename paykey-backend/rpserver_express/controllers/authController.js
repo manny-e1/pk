28e02497264b05a6f668a2778a1860ad5ad133f9
anjar <anjarvery@gmail.com>
2026-02-09 19:40:09 +0700

    update authController.js and authService.ts

--- paykey-backend/rpserver_express/controllers/authController.js
+++ paykey-backend/rpserver_express/controllers/authController.js
@@ -147,8 +147,20 @@ exports.checkUser = async (req, res) => {
     } catch (err) { res.status(500).json({ error: err.message }); }
 };
 
+// exports.logout = (req, res) => {
+//     res.cookie('auth_token', 'none', { expires: new Date(Date.now() + 1000), httpOnly: true, path: '/' });
+//     res.clearCookie('auth_token');
+//     res.status(200).json({ success: true });
+// };
+
 exports.logout = (req, res) => {
-    res.cookie('auth_token', 'none', { expires: new Date(Date.now() + 1000), httpOnly: true, path: '/' });
-    res.clearCookie('auth_token');
-    res.status(200).json({ success: true });
+    res.clearCookie('auth_token', {
+        httpOnly: true,
+        secure: true, 
+        sameSite: 'lax',
+        domain: '.authkey.my',
+        path: '/'
+    });
+
+    res.status(200).json({ message: 'Logged out successfully' });
 };
