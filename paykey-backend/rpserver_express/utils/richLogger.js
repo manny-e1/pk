a4d0fbabaa8f5c838aa3e6c6067fb2a5255a44d2
anjar <anjarvery@gmail.com>
2026-02-12 22:14:20 +0700

    update passkeyController.js and richLogger.js

--- paykey-backend/rpserver_express/utils/richLogger.js
+++ paykey-backend/rpserver_express/utils/richLogger.js
@@ -8,6 +8,9 @@ const generateEventID = () => `evt__${customAlphabet('0123456789ABCDEF', 10)()}`
 async function createRichAuthLog(req, user, context) {
     try {
         const ip = req.headers['x-forwarded-for'] || req.socket.remoteAddress;
+        if (ip.includes(',')) {
+            ip = ip.split(',')[0].trim();
+        }
         const userAgentString = req.headers['user-agent'] || '';
 
         // 1. Parse User Agent
