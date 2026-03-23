2e512420387a71fcb21b60c0c3ae6a4ba15b66ac
anjar <anjarvery@gmail.com>
2026-03-24 04:39:42 +0700

    update 18 files and delete 1 file

--- paykey-frontend/.next/dev/server/chunks/ssr/[root-of-the-server]__54308bc6._.js
+++ paykey-frontend/.next/dev/server/chunks/ssr/[root-of-the-server]__54308bc6._.js
@@ -117,7 +117,7 @@ __turbopack_context__.s([
 var __TURBOPACK__imported__module__$5b$project$5d2f$node_modules$2f$axios$2f$lib$2f$axios$2e$js__$5b$app$2d$ssr$5d$__$28$ecmascript$29$__ = __turbopack_context__.i("[project]/node_modules/axios/lib/axios.js [app-ssr] (ecmascript)");
 ;
 const apiClient = __TURBOPACK__imported__module__$5b$project$5d2f$node_modules$2f$axios$2f$lib$2f$axios$2e$js__$5b$app$2d$ssr$5d$__$28$ecmascript$29$__["default"].create({
-    baseURL: `${("TURBOPACK compile-time value", "https://api.authkey.my")}/admin` || 'https://api.authkey.my',
+    baseURL: `${("TURBOPACK compile-time value", "http://localhost:4000")}/admin` || 'https://api.authkey.my',
     withCredentials: true,
     headers: {
         'Content-Type': 'application/json'
